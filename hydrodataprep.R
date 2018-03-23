0c949f5088b0e65c7597b4a937d777150207fe76
Mathis Loïc Messager <messamat@uw.edu>
2018-03-22 18:06:36 -0700

    Relativize path for data

--- hydrodataprep.R
+++ hydrodataprep.R
@@ -12,35 +12,35 @@
 
 library(openxlsx)
 library(reshape2)
-setwd("F:/Tanzania/Tanzania/results")
-
+setwd("F:/Tanzania/Tanzania/results") #UPDATE
+datadir = "F:/Tanzania/Tanzania/data" #UPDATE
 ########################################
 #Import and merge hydrological data
 ########################################
 #Import and merge ruaha data from Zach 
-ruahaflow <- read.csv("F:/Tanzania/Tanzania/data/sharepoint20180316/flow/2018-03-06 Corrected Ruaha Stage and Flow_data.csv")
+ruahaflow <- read.csv(file.path(datadir,"sharepoint20180316/flow/2018-03-06 Corrected Ruaha Stage and Flow_data.csv"))
 length(table(ruahaflow$Gage.ID))
 ruahaflow <- ruahaflow[,!names(ruahaflow) %in% c("X","X.1")]
 #str(ruahaflow)
-ruahastations <- read.csv("F:/Tanzania/Tanzania/data/sharepoint20180316/flow/2018-03-06 Corrected Ruaha Stage and Flow_stations.csv")
+ruahastations <- read.csv(file.path(datadir,"sharepoint20180316/flow/2018-03-06 Corrected Ruaha Stage and Flow_stations.csv"))
 #str(ruahastations)
 ruahadat <- merge(ruahaflow, ruahastations[,c('Index','Station_ID','Station_Name','Rating.Curve.Source','Record.Quality','Rating.Curve.Quality')], 
                   by.x='Gage.ID', by.y='Station_ID')
 str(ruahadat)
 
 #Import and merge Kilombero data
-kilomflow <- read.csv("F:/Tanzania/Tanzania/data/sharepoint20180316/flow/2018-03-13 Corrected Kilombero Stage and Flow_data.csv")
+kilomflow <- read.csv(file.path(datadir,"sharepoint20180316/flow/2018-03-13 Corrected Kilombero Stage and Flow_data.csv"))
 kilomflow <- kilomflow[,!names(kilomflow) %in% c("X","X.1","X.2","X.3","X.4")]
 str(kilomflow)
 colnames(kilomflow) <- colnames(ruahaflow)
 length(table(kilomflow$Gage.ID))
-kilomstations <- read.csv("F:/Tanzania/Tanzania/data/sharepoint20180316/flow/2018-03-13 Corrected Kilombero Stage and Flow_stations.csv")
+kilomstations <- read.csv(file.path(datadir,"sharepoint20180316/flow/2018-03-13 Corrected Kilombero Stage and Flow_stations.csv"))
 str(kilomstations)
 kilomdat <- merge(kilomflow, kilomstations, by.x='Gage.ID', by.y='Station_ID')
 rufidat <- rbind(ruahadat, kilomdat)
 
 #Import daily data from Japhet K.
-Japhet_GRuaha <- read.xlsx("F:/Tanzania/Tanzania/data/JaphetK_20180316/Rufiji_L.RukwaBasins_Flow Data_formatMM20180322.xlsx",sheet=1,startRow=4, detectDates=T)
+Japhet_GRuaha <- read.xlsx(file.path(datadir,"JaphetK_20180316/Rufiji_L.RukwaBasins_Flow Data_formatMM20180322.xlsx"),sheet=1,startRow=4, detectDates=T)
 Japhet_1KA59 <- Japhet_GRuaha[,c(1,2)]
 Japhet_1KA59[,'Station'] <- '1KA59'
 Japhet_1KA11 <- Japhet_GRuaha[,c(3,4)]
@@ -50,7 +50,7 @@ Japhet_GRuaha <- Japhet_GRuaha[!is.na(Japhet_GRuaha$Date) & Japhet_GRuaha$`Flow.
 Japhet_GRuaha$`Flow.(m3/s)` <- as.numeric(Japhet_GRuaha$`Flow.(m3/s)`)
 str(Japhet_GRuaha)
 
-Japhet_Kilom <- read.xlsx("F:/Tanzania/Tanzania/data/JaphetK_20180316/Rufiji_L.RukwaBasins_Flow Data_formatMM20180322.xlsx",sheet=2,startRow=3, detectDates=T)
+Japhet_Kilom <- read.xlsx(file.path(datadir,"JaphetK_20180316/Rufiji_L.RukwaBasins_Flow Data_formatMM20180322.xlsx"),sheet=2,startRow=3, detectDates=T)
 Japhet_Kilom <- Japhet_Kilom[-1,]
 colnames(Japhet_Kilom)[c(1,6)] <- c('Date','Udagaji')
 Japhet_Kilom <- melt(Japhet_Kilom, id.vars = 'Date',value.name='Flow.(m3/s)',variable.name='Station')
@@ -59,7 +59,7 @@ Japhet_Kilom$`Flow.(m3/s)` <- as.numeric(Japhet_Kilom$`Flow.(m3/s)`)
 Japhet_Kilom$Date <- as.Date(Japhet_Kilom$Date)
 Japhet_Kilom <- Japhet_Kilom[!is.na(Japhet_Kilom$`Flow.(m3/s)`),]
 
-Japhet_LRuaha <- read.xlsx("F:/Tanzania/Tanzania/data/JaphetK_20180316/Rufiji_L.RukwaBasins_Flow Data_formatMM20180322.xlsx",sheet=3,startRow=3, detectDates=T)
+Japhet_LRuaha <- read.xlsx(file.path(datadir,"JaphetK_20180316/Rufiji_L.RukwaBasins_Flow Data_formatMM20180322.xlsx"),sheet=3,startRow=3, detectDates=T)
 Japhet_LRuaha <- Japhet_LRuaha[-1,]
 colnames(Japhet_LRuaha)[1] <- 'Date'
 Japhet_LRuaha <- melt(Japhet_LRuaha, id.vars = 'Date',value.name='Flow.(m3/s)',variable.name='Station')
@@ -71,7 +71,7 @@ Japhet_LRuaha <- Japhet_LRuaha[!is.na(Japhet_LRuaha$`Flow.(m3/s)`),]
 Japhet_dailydat <- rbind(Japhet_GRuaha, Japhet_Kilom, Japhet_LRuaha)
 
 #Import monthly unimpaired data from Japhet K.
-Japhet_Unimpaired <- read.xlsx("F:/Tanzania/Tanzania/data/JaphetK_20180316/Rufiji_L.RukwaBasins_Flow Data_formatMM20180322.xlsx",sheet=4,startRow=2, detectDates=T)
+Japhet_Unimpaired <- read.xlsx(file.path(datadir,"JaphetK_20180316/Rufiji_L.RukwaBasins_Flow Data_formatMM20180322.xlsx"),sheet=4,startRow=2, detectDates=T)
 Japhet_Unimpaired <- Japhet_Unimpaired[-1,]
 Japhet_Unimpaired <- melt(Japhet_Unimpaired, id.vars = 'Date',value.name='Flow.(m3/s)',variable.name='Station')
 str(Japhet_Unimpaired)
@@ -79,7 +79,7 @@ Japhet_Unimpaired$`Flow.(m3/s)` <- as.numeric(Japhet_Unimpaired$`Flow.(m3/s)`)
 Japhet_Unimpaired <- Japhet_Unimpaired[!is.na(Japhet_Unimpaired$`Flow.(m3/s)`),] #No NA, must be modeled?
 
 #Import monthly Rukwa data from Japhet K.
-Japhet_Rukwa <- read.xlsx("F:/Tanzania/Tanzania/data/JaphetK_20180316/Rufiji_L.RukwaBasins_Flow Data_formatMM20180322.xlsx",sheet=5,startRow=2, detectDates=T)
+Japhet_Rukwa <- read.xlsx(file.path(datadir,"JaphetK_20180316/Rufiji_L.RukwaBasins_Flow Data_formatMM20180322.xlsx"),sheet=5,startRow=2, detectDates=T)
 Japhet_Rukwa <- Japhet_Rukwa[-1,c(-3,-5)]
 colnames(Japhet_Rukwa) <- c('Date','3CD2','3B2','3A17')
 Japhet_Rukwa <- melt(Japhet_Rukwa, id.vars = 'Date',value.name='Flow.(m3/s)',variable.name='Station')
