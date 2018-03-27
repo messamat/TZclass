5360528244d0eadca22c0eff4727eaec99cfa3da
Mathis Loïc Messager <messamat@uw.edu>
2018-03-26 20:57:13 -0700

    Compute max gap length

--- hydrodataprep4.R
+++ hydrodataprep4.R
@@ -13,7 +13,7 @@
 library(ggplot2)
 library(data.table)
 library(FlowScreen) #to inspect data
-library(compare)
+library(reshape)
 setwd("F:/Tanzania/Tanzania/results") #UPDATE
 datadir = file.path(getwd(),paste('rufiji_hydrodatainspect','20180326',sep='_')) #UPDATE
 origdatadir = "F:/Tanzania/Tanzania/data"
@@ -26,16 +26,9 @@ rufidat_deleted <- read.csv(file.path(datadir,'rufidat_deleted.csv'), colClasses
 #BUild summary tables
 rufidat_clean$year <- as.numeric(format(rufidat_clean$Date, "%Y"))
 rufidat_clean$month <- as.numeric(format(rufidat_clean$Date, "%m"))
-rufidat_clean$prevdate <- NA #Get previous date of record for that station for each date
-for (i in 2:nrow(rufidat_clean)){
-  if (rufidat_clean[i,'ID']==rufidat_clean[i-1,'ID']) {
-    rufidat_clean[i,'prevdate'] < rufidat_clean[i-1,'Date']
-  }
-}
-
 rufidat_dt <- data.table(rufidat_clean)
-rufidat_datesummary <- rufidat_dt[,list(length(unique(ID))), .(Date)]
 
+rufidat_datesummary <- rufidat_dt[,list(length(unique(ID))), .(Date)] #Compute number of gages with data for each date
 ggplot(data=rufidat_clean, aes(x=Date, y=ID)) + 
   geom_bar(data=rufidat_datesummary, aes(x=Date,y='1KB9',color=V1), stat='identity') +
   geom_point() +
@@ -43,25 +36,27 @@ ggplot(data=rufidat_clean, aes(x=Date, y=ID)) +
   scale_colour_distiller('spectral') +
   theme_bw()
 
+#Compute length of gap between current daily record and previous record both within and between years for each date and gage
+rufidat_dt[, prevdate := ifelse(shift(ID, 1L, type="lag")==ID,shift(as.character(Date), 1L, type="lag"), NA)]
+rufidat_dt[, prevdateyr := ifelse((shift(ID, 1L, type="lag")==ID) & (shift(year, 1L, type="lag")==year),shift(as.character(Date), 1L, type="lag"), NA)]
+rufidat_dt$prevgap <- as.numeric(rufidat_dt$Date-as.Date(rufidat_dt$prevdate))
+rufidat_dt$prevgapyr <- as.numeric(rufidat_dt$Date-as.Date(rufidat_dt$prevdateyr))
+
+#Compute summary statistics in terms of amount of record, percentage of gaps, max length of gaps both
+#regarding entire record
 rufidat_summary <- rufidat_dt[,list(min_year=min(year), max_year=max(year), max_len=max(year)-min(year), 
                                     max_lend=max(Date)-min(Date), gap_d=as.numeric((max(Date)-min(Date)))-length(unique(Date)), gap_per=1-(length(unique(Date))/as.numeric((max(Date)-min(Date)))),
-                                    max_gap = max(Date-prevdate),
-                                       ), .(ID)]
-
-rufidat_gapsummary <- rufidat_dt[,list(gap_d=as.numeric(format(as.Date(paste(year, "12", "31", sep="-")), "%j"))-len(unique(Date)),
-                                          gap_per=length(unique(Date))/as.numeric(format(as.Date(paste(year, "12", "31", sep="-")), "%j")),
-                                          ), .(ID,year)]
-
-
-#Test range of maximum gap length per year and total percentage missing data
+                                    max_gap = max(prevgap,na.rm=T)
+                                    ),.(ID)]
+#regardomg yearly record
+rufidat_gapsummary <- rufidat_dt[,list(gap_d=as.numeric(format(as.Date(paste(year, "12", "31", sep="-")), "%j"))-length(unique(Date)),
+                                          gap_per=1-(length(unique(Date))/as.numeric(format(as.Date(paste(year, "12", "31", sep="-")), "%j"))),
+                                          max_gap = max(prevgapyr,na.rm=T)
+                                          ),.(ID,year)]
 
 #####
-
 #To do:
-#Get summary statistics on 
-# grain of data
-# length of record
-# completeness in terms of frequency, length, and time periods of gaps
-# overlap in terms of period and length
-#Test range of acceptance criteria (15 years, etc.)
+#Assess number of gages available based on different minimum number of years of record, gap percentage, and maximum gap size
+#Based on these requirements, assess number of gages available based on required length of overlap.
+#Based on these requirements, assess number of gages based on non-stationarity
 #Evaluate consistency of discharge with drainage area and precipitation + HydroSHEDS modeled data
